bc1e54243b70935350afd057870c04a56a2fec32
kskkry <koskar0358@gmail.com>
2022-11-02 00:25:02 +0900

    create overlap.py

---
+++ python/overlap.py
@@ -0,0 +1,7 @@
+import numpy as np
+from geometry import Atom
+
+def compute_overlap(alpha1:float, l1:int,m1:int, n1:int, atom1: Atom, alpha2:float, l2:int, m2:int, n2:int, atom2:Atom):
+
+
+def compute_gto_overlap()
