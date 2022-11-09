2a2d670c5ed44432fbd8ec78851d794c26cbbb6c
kryksk <koskar0358@gmail.com>
2022-11-09 12:53:47 +0900

    rename

---
+++ python/geometry.py
@@ -0,0 +1,29 @@
+from distutils.command.config import config
+import numpy as np
+from config import Config
+cfg = Config()
+
+class Vec(object):
+    def __init__(self,x,y,z) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def getx(self):
+        return self.x
+
+    def gety(self):
+        return self.y
+
+    def getz(self):
+        return self.z
+
+
+class Atom(Vec):
+    def __init__(self,str_element,x,y,z) -> None:
+        super().__init__(x,y,z)
+        self.element = cfg.get_element_num()[str_element]
+    
+class Geometry(object):
+    def __init__(self, atom_list:list):
+        pass
